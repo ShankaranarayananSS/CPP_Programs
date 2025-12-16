7c374946a3aee3df1a5bf6053da8c80212e7cacc
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-16 08:35:59 +0530

    Create Arithmetic Operations

---
+++ Operations/Arithmetic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	float n1,n2;
+	cout<<"Enter number-1:";
+	cin>>n1;
+	cout<<"Enter number-2:";
+	cin>>n2;
+	
+	cout<<n1<<" + "<<n2<<" = "<<n1+n2<<endl;
+	cout<<n1<<" - "<<n2<<" = "<<n1-n2<<endl;
+	cout<<n1<<" * "<<n2<<" = "<<n1*n2<<endl;
+	cout<<n1<<" / "<<n2<<" = "<<n1/n2<<endl;
+}
