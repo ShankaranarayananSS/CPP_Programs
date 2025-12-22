002850a08390fec2501d334197277933ea62c292
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-22 17:49:32 +0530

    Create Square

---
+++ Simple Programs/Square.cpp
@@ -0,0 +1,12 @@
+//	Perimeter of Square 	: 4 * Side
+//	Area of Square			: Side * Side
+
+#include<iostream>
+using namespace std;
+int main(){
+	float s;
+	cout<<"Enter Side of Square:";
+	cin>>s;
+	cout<<"\nPerimeter of Square: "<<4*s;
+	cout<<"\nArea of Square:"<<s*s;
+}
