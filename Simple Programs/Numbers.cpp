f1b82bebbb9921f27f5939f6006580247489bcca
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-22 17:52:39 +0530

    Create Numbers

---
+++ Simple Programs/Numbers.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"Enter any number:";
+	cin>>num;
+	cout<<"\nSquare of number: "<<num*num;
+	cout<<"\nCube of number: "<<num*num*num;
+}
