3aff64308a9f6bbf28f59d4d9729b21f532947f3
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-22 17:55:34 +0530

    Create Marks

---
+++ Simple Programs/Marks.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int m1,m2,m3,m4,m5,total;
+	float avg;
+	cout<<"Enter mark-1:";
+	cin>>m1;
+	cout<<"Enter mark-2:";
+	cin>>m2;
+	cout<<"Enter mark-3:";
+	cin>>m3;
+	cout<<"Enter mark-4:";
+	cin>>m4;
+	cout<<"Enter mark-5:";
+	cin>>m5;
+	total = m1+m2+m3+m4+m5;
+	avg = total/5;
+	cout<<"\nTotal Marks: "<<total;
+	cout<<"\nAverage Marks: "<<avg;
+}
