126c750631a51cbeb5089be9912accd48343ae2e
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-22 19:41:32 +0530

    Create Triangle

---
+++ Simple Programs/Triangle.cpp
@@ -0,0 +1,12 @@
+//Area of Right Angle Triangle	: 1/2 * Base * Height
+
+#include<iostream>
+using namespace std;
+int main(){
+	float b,h;
+	cout<<"Enter Base of Triangle:";
+	cin>>b;
+	cout<<"Enter Height of Triangle:";
+	cin>>h;
+	cout<<"Area of Triangle:"<<b*h/2;
+}
