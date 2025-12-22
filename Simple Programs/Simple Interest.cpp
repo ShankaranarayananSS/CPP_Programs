5ddc532a5e8de458590b52c959644423f9d73b3c
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-22 19:38:33 +0530

    Create Simple Interest

---
+++ Simple Programs/Simple Interest.cpp
@@ -0,0 +1,14 @@
+//Simple Interest	: (Principal Amount * No. of Years * Rate of Interest) / 100
+
+#include<iostream>
+using namespace std;
+int main(){
+	float p,n,r;
+	cout<<"Enter Principal amount:";
+	cin>>p;
+	cout<<"Enter No. of Years:";
+	cin>>n;
+	cout<<"Enter Principal amount:";
+	cin>>r;
+	cout<<"Simple Interest: "<<p*n*r/100;
+}
