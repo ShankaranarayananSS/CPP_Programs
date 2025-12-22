03c214631a01c602c68186f74dff46c0a02b9dc2
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-22 17:45:23 +0530

    Create Circle

---
+++ Simple Programs/Circle.cpp
@@ -0,0 +1,12 @@
+//	Perimeter of Circle	: 2 * Pie * Radius
+//	Area of Circle		: Pie * Radius * Radius
+
+#include<iostream>
+using namespace std;
+int main(){
+	float r;
+	cout<<"Enter radius of a Circle:";
+	cin>>r;
+	cout<<"\nPerimeter of Circle: "<<2*3.14*r;
+	cout<<"\nArea of Circle: "<<3.14*r*r;
+}
