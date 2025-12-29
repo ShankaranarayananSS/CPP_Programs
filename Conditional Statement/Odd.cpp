a1782ab2ed92c80c1b45443c17ec924f6494be27
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-29 22:12:07 +0530

    Create Odd

---
+++ Conditional Statement/Odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"Enter any number:";
+	cin>>num;
+	if(num%2==0){
+		cout<<num<<" is Even number!";
+	}
+	else{
+		cout<<num<<" is Odd number!";
+	}
+}
