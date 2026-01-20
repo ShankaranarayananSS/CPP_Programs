a33d67d0fb98638b70cc5dd8b39a76416049a014
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-20 22:26:46 +0530

    Create Temperature Conversion Program 1

---
+++ Simple Programs/Temperature1.cpp
@@ -0,0 +1,12 @@
+//Temperature Conversion
+//Degree Celsius to Fahrenheit
+//F = (C*9/5) + 32
+
+#include<iostream>
+using namespace std;
+int main(){
+  float f,c;
+  cout<<"Enter Celsius:";
+  cin>>c;
+  printf("Fahrenheit = %.2f",(c*9/5)+32);
+}
