ee2085bfa5eac4a18194f8b9cfefc614528e297b
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-24 18:19:28 +0530

    Create Temperature Conversion Program 3

---
+++ Simple Programs/Temperature3.cpp
@@ -0,0 +1,12 @@
+//Temperature Conversion
+//Degree Celsius to Kelvin
+//K = C + 273.15
+
+#include<iostream>
+using namespace std;
+int main(){
+  float c;
+  cout<<"Enter Celsius:";
+  cin>>c;
+  printf("Kelvin = %.2f",c+273.15);
+}
