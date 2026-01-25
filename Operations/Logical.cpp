488f10d2d374d8d00fafa46348e4d378e74c6ae3
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-25 15:12:29 +0530

    Create Logical Operations

---
+++ Operations/Logical.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+  int t=1, f=0;
+  cout<<"Truth Tables:"<<endl;
+  
+  cout<<"\nAND OPERATOR:"<<endl;
+  cout<<t<<" && "<<t<<" = "<<(t&&t)<<endl;
+  cout<<t<<" && "<<f<<" = "<<(t&&f)<<endl;
+  cout<<f<<" && "<<t<<" = "<<(f&&t)<<endl;
+  cout<<f<<" && "<<f<<" = "<<(f&&f)<<endl;
+  
+  cout<<"\nOR OPERATOR:"<<endl;
+  cout<<t<<" || "<<t<<" = "<<(t||t)<<endl;
+  cout<<t<<" || "<<f<<" = "<<(t||f)<<endl;
+  cout<<f<<" || "<<t<<" = "<<(f||t)<<endl;
+  cout<<f<<" || "<<f<<" = "<<(f||f)<<endl;
+  
+  cout<<"\nNOT OPERATOR:"<<endl;
+  cout<<"!"<<t<<" = "<<(!t)<<endl;
+  cout<<"!"<<f<<" = "<<(!f)<<endl;
+}
