fe6f4bb186b2d33215a7d7b8bf54c6b088fa7c53
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-25 09:42:08 +0530

    Create Factorial For Loop

---
+++ Looping Statement/For Loop/Factorial.cpp
@@ -0,0 +1,14 @@
+//For Loop
+//Factorial
+
+#include<iostream>
+using namespace std;
+int main(){
+  int i,n,fact=1;
+  cout<<"Enter any number:";
+  cin>>n;
+  for(i=n;i>=1;i--){
+    fact = fact*i;
+  }
+  cout<<"Factorial of "<<n<<" is "<<fact;
+}
