893ec4e8dfe743e7e6edb74abf6c662e4b93b8de
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-25 10:01:18 +0530

    Create Factorial Do While Loop

---
+++ Looping Statement/Do While/Factorial.cpp
@@ -0,0 +1,17 @@
+//While Loop
+//Factorial
+
+#include<iostream>
+using namespace std;
+int main(){
+  int num,i=1,fact=1;
+	cout<<"Enter any number:";
+  cin>>num;
+  do{
+    fact = fact*i;
+    i++;
+  }
+  while(i<=num);{
+    cout<<"Factorial of "<<num<<" is "<<fact;	
+  }
+}
