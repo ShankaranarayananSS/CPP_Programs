8f6fd3f28f9d123e18e18698df0e1d42484b866f
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-28 22:25:01 +0530

    Create Vowels

---
+++ Operations/Vowels.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  char letter;
+  cout<<"Enter any letter:";
+  cin>>letter;
+  if(letter=='A' || letter=='E' || letter=='I' || letter=='O' || letter=='U' ||
+    letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter=='u'){
+    cout<<"'"<<letter<<"\' is a Vowel!";
+  }
+  else{
+    cout<<"\'"<<letter<<"\' is a Consonant or Other character";
+	}
+}
