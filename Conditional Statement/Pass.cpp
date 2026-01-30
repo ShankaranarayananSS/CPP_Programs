1cd0370f9eee8610269dbbc1ebe094442f67a0ad
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-30 08:21:30 +0530

    Create Pass or Fail

---
+++ Conditional Statement/Pass.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float percent;
+  cout<<"Enter your percentage:";
+  cin>>percent;
+  if(percent>=40){
+    cout<<"Pass!";
+  }
+  else{
+    cout<<"Fail!";
+  }
+}
