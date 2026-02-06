0dc876c8191f286d0e635390202c60f37e402f80
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-06 22:50:22 +0530

    Update Leap Year

--- Conditional Statement/Leap Year.cpp
+++ Conditional Statement/Leap Year.cpp
@@ -4,7 +4,7 @@ int main(){
 	int year;
 	cout<<"Enter any year:";
 	cin>>year;
-	if(year%4==0){
+	if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
 		cout<<year<<" is Leap year!";
 	}
 	else{
