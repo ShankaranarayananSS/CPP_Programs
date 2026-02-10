614c01a7919d8b3df81b9439a92f21266457755c
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-10 23:11:38 +0530

    Update Vowels

--- Conditional Statement/Vowels.cpp
+++ Conditional Statement/Vowels.cpp
@@ -6,7 +6,7 @@ int main(){
   cin>>letter;
   if(letter=='A' || letter=='E' || letter=='I' || letter=='O' || letter=='U' ||
     letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter=='u'){
-    cout<<"'"<<letter<<"\' is a Vowel!";
+    cout<<"\'"<<letter<<"\' is a Vowel!";
   }
   else{
     cout<<"\'"<<letter<<"\' is a Consonant or Other character";
