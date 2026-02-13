9522ae08253baa8e9bc5c4f002f8f08f56015436
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-14 00:07:54 +0530

    Update Bitwise Operations

--- Operations/Bitwise.cpp
+++ Operations/Bitwise.cpp
@@ -11,6 +11,6 @@ int main(){
 	cout<<num1<<" ^ "<<num2<<" = "<<(num1^num2)<<endl;
 	cout<<"~"<<num1<<" = "<<(~num1)<<endl;
 	cout<<"~"<<num2<<" = "<<(~num2)<<endl;
-	cout<<num1<<">>1"<<(num1>>1)<<endl;
-	cout<<num1<<"<<1"<<(num1<<1)<<endl;
+	cout<<num1<<">>1 = "<<(num1>>1)<<endl;
+	cout<<num1<<"<<1 = "<<(num1<<1)<<endl;
 }
