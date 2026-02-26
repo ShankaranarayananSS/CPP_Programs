5c780a8384654b5a74f5134d5d169d7867761f2e
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-26 23:33:18 +0530

    Update Arithmetic Operations

--- Operations/Arithmetic1.cpp
+++ Operations/Arithmetic1.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-	
 	float n1,n2;
 	cout<<"Enter number-1:";
 	cin>>n1;
@@ -12,9 +11,5 @@ int main(){
 	cout<<n1<<" - "<<n2<<" = "<<n1-n2<<endl;
 	cout<<n1<<" * "<<n2<<" = "<<n1*n2<<endl;
 	cout<<n1<<" / "<<n2<<" = "<<n1/n2<<endl;
-	
-	int num1 = n1;
-	int num2 = n2;
-	int mod = num1%num2;
-	cout<<n1<<" % "<<n2<<" = "<<mod<<endl;
+	return 0;
 }
