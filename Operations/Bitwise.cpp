080642f523645720acacc53829b883d7dd4358c8
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-27 23:00:53 +0530

    Update Bitwise Operations

--- Operations/Bitwise.cpp
+++ Operations/Bitwise.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-	
 	int num1,num2;
 	cout<<"Enter two integers:";
 	cin>>num1>>num2;
@@ -13,4 +12,7 @@ int main(){
 	cout<<"~"<<num2<<" = "<<(~num2)<<endl;
 	cout<<num1<<">>1 = "<<(num1>>1)<<endl;
 	cout<<num1<<"<<1 = "<<(num1<<1)<<endl;
+	cout<<num2<<">>1 = "<<(num2>>1)<<endl;
+	cout<<num2<<"<<1 = "<<(num2<<1)<<endl;
+	return 0;
 }
