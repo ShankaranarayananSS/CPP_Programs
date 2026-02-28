d62a8847b60508688430fc08058258402d5c96cc
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-28 23:25:42 +0530

    Update Increment Operations

--- Operations/Increment.cpp
+++ Operations/Increment.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-	
 	int i;
 	cout<<"Enter any integer:";
 	cin>>i;
@@ -15,4 +14,5 @@ int main(){
 	cout<<i<<endl;
 	cout<<i--<<endl;
 	cout<<--i<<endl;
+	return 0;
 }
