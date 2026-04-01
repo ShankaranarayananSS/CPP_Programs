0e64865f9685aec3226c414ac8f8c78f84597c84
TAMILNADU COMPUTER SOFTWARE COLLEGE <272538512+TNCSC-TRICHY@users.noreply.github.com>
2026-04-01 17:22:30 +0530

    Update Factorial

--- Looping Statement/Do While/Factorial.cpp
+++ Looping Statement/Do While/Factorial.cpp
@@ -4,14 +4,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int num,i=1,fact=1;
+	int num,i=1,fact=1;
 	cout<<"Enter any number:";
-  cin>>num;
-  do{
-    fact = fact*i;
-    i++;
-  }
-  while(i<=num);{
-    cout<<"Factorial of "<<num<<" is "<<fact;	
-  }
+	cin>>num;
+	
+	do{
+		fact = fact*i;
+		i++;
+	}
+	while(i<=num);{
+		cout<<"Factorial of "<<num<<" is "<<fact;
+	}
+	return 0;
 }
