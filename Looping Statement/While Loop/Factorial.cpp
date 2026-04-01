e0843bf9ce605708844af254b6346a9be455dbfa
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-04-01 12:13:29 +0530

    Update Factorial

--- Looping Statement/While Loop/Factorial.cpp
+++ Looping Statement/While Loop/Factorial.cpp
@@ -7,9 +7,11 @@ int main(){
   int num,i=1,fact=1;
   cout<<"Enter any number:";
   cin>>num;
+  
   while(i<=num){
     fact = fact*i;
     i++;
   }
-  cout<<"Factorial of "<<num<<" is "<<fact;	
+  cout<<"Factorial of "<<num<<" is "<<fact;
+  return 0;
 }
