55d31c16b01cfe5a1287f1a2a1a4e4f8b0af813b
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-05-04 23:10:17 +0530

    Create Relational Operator

---
+++ Operator/Relational.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n1,n2;
+  cout<<"Enter number-1:";
+  cin>>n1;
+  cout<<"Enter number-2:";
+  cin>>n2;
+  
+  cout<<endl<<n1<<" < "<<n2<<" : "<<(n1<n2);
+  cout<<endl<<n1<<" > "<<n2<<" : "<<(n1>n2);
+  cout<<endl<<n1<<" <= "<<n2<<" : "<<(n1<=n2);
+  cout<<endl<<n1<<" >= "<<n2<<" : "<<(n1>=n2);
+  cout<<endl<<n1<<" == "<<n2<<" : "<<(n1==n2);
+  cout<<endl<<n1<<" != "<<n2<<" : "<<(n1!=n2);
+  return 0;
+}
