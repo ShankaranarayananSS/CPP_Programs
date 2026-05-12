0a334ae700b64e1b3f75540e0081f440ca360285
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-05-12 23:29:52 +0530

    Update Square

---
+++ Simple Programs/Square.cpp
@@ -0,0 +1,14 @@
+//Area of Square       =  S * S
+//Perimeter of Square  =  4 * S
+
+#include<iostream>
+using namespace std;
+int main(){
+  int s;
+  cout<<"Enter side of square:";
+  cin>>s;
+  
+  cout<<endl<<"Area of Square = "<<(s*s);
+  cout<<endl<<"Perimeter of Square = "<<(4*s);
+  return 0;
+}
