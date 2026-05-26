99aaef739c4d93bea67e5816b40df4ffd77e701c
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-05-26 22:09:47 +0530

    Create Positive or Negative

---
+++ Conditional Statement/If Else Ladder/Positive.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter number:";
+  cin>>n;
+  
+  if(n>0){
+    cout<<"Positive number!";
+  }else if(n<0){
+    cout<<"Negative number!";
+  }else{
+    cout<<"Neutral";
+  }
+  return 0;
+}
