0e8009c676d6554af39883c133118498c9237182
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-06-03 22:27:13 +0530

    Create Pass or Fail V2

---
+++ Operator/Ternary/Pass2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float percent;
+  cout<<"Enter your percentage:";
+  cin>>percent;
+  
+  (percent<40)?cout<<"Fail":cout<<"Pass";
+  return 0;
+}
