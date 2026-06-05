968209c8206490b2d8daa4ff35f3de3b82f444be
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-06-05 23:51:43 +0530

    Create Triangle

---
+++ Simple Programs/Triangle.cpp
@@ -0,0 +1,14 @@
+//Area of Triangle  = 0.5 * B * H
+
+#include<iostream>
+using namespace std;
+int main(){
+  float b,h;
+  cout<<"Enter base of triangle:";
+  cin>>b;
+  cout<<"Enter height of triangle:";
+  cin>>h;
+  
+  cout<<endl<<"Area of Triangle = "<<(0.5*b*h);
+  return 0;
+}
