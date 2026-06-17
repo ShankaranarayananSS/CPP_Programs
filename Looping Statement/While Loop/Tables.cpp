3ae0a1f7828960d47497fd9a785f61fc01269409
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-06-17 22:52:15 +0530

    Create Multiplication Tables While Loop

---
+++ Looping Statement/While Loop/Tables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num,i=1;
+  cout<<"Enter number:";
+  cin>>num;
+  
+  cout<<num<<" Tables:";
+  while(i<=10){
+    cout<<endl<<num<<" * "<<i++<<" = "<<num*i;
+  }
+  return 0;
+}
