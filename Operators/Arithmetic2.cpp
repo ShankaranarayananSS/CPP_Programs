12a5d846a95f4a7adbb27a56e6c0d9f3ebbef3d3
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-13 21:13:54 +0530

    Create Arithmetic Operator

---
+++ Operators/Arithmetic2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n1,n2;
+  cout<<"Enter number-1:";
+  cin>>n1;
+  cout<<"Enter number-2:";
+  cin>>n2;
+  
+  cout<<n1<<" % "<<n2<<" = "<<(n1%n2);
+  return 0;
+}
