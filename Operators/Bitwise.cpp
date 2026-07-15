7d0f3ec47c76f7f3909b2e2d457bea2cc9fddf46
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-15 08:25:04 +0530

    Create Bitwise Operator

---
+++ Operators/Bitwise.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n1,n2;
+  cout<<"Enter number-1:";
+  cin>>n1;
+  cout<<"Enter number-2:";
+  cin>>n2;
+  
+  cout<<endl<<n1<<" & "<<n2<<" = "<<(n1&n2);
+  cout<<endl<<n1<<" | "<<n2<<" = "<<(n1|n2);
+  cout<<endl<<n1<<" ^ "<<n2<<" = "<<(n1^n2);
+  cout<<endl<<"~"<<n1<<" = "<<~n1;
+  cout<<endl<<"~"<<n2<<" = "<<~n2;
+  return 0;
+}
