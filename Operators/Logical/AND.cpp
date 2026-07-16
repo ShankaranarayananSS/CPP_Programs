f6154e1e5dca80e03c7f6a62d8bc7ba941b77169
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-16 22:44:56 +0530

    Create Logical AND

---
+++ Operators/Logical/AND.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int f=0,t=1;
+  cout<<"Logical AND";
+  cout<<endl<<f<<" && "<<f<<" = "<<(f&&f);
+  cout<<endl<<f<<" && "<<t<<" = "<<(f&&t);
+  cout<<endl<<t<<" && "<<f<<" = "<<(t&&f);
+  cout<<endl<<t<<" && "<<t<<" = "<<(t&&t);
+  return 0;
+}
