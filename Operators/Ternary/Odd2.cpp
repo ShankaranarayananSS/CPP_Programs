81ac9243d61a4941d468e4d9bd78d4847e9194d4
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-18 23:25:35 +0530

    Create Odd Or Even Version 2

---
+++ Operators/Ternary/Odd2.cpp
@@ -0,0 +1,12 @@
+//Odd Or Even - Version 2
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter number:";
+  cin>>n;
+  
+  (n%2==1)?cout<<"Odd":cout<<"Even";
+  return 0;
+}
