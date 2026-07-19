0f28f6ea4da69c9a79a997be8e08d778b97aabcd
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-19 20:52:22 +0530

    Create Vote Eligibility Version 2

---
+++ Operators/Ternary/Vote2.cpp
@@ -0,0 +1,12 @@
+//Vote Eligibility - Version 2
+
+#include<iostream>
+using namespace std;
+int main(){
+  int age;
+  cout<<"Enter age:";
+  cin>>age;
+  
+  (age<18)?cout<<"Eligible":cout<<"Not eligible";
+  return 0;
+}
