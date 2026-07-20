0fd81ae54cff067416251052f4563f01013beef5
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-20 22:45:56 +0530

    Create Pass Or Fail Version 1

---
+++ Operators/Ternary/Pass1.cpp
@@ -0,0 +1,12 @@
+//Pass Or Fail - Version 1
+
+#include<iostream>
+using namespace std;
+int main(){
+  int mark;
+  cout<<"Enter mark:";
+  cin>>mark;
+  
+  (mark>=40)?cout<<"Pass":cout<<"Fail";
+  return 0;
+}
