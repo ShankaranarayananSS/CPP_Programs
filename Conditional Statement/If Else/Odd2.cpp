668abb534b7d97b0a2a663d983051db3974116a9
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-23 22:13:36 +0530

    Create Odd Or Even Version 2

---
+++ Conditional Statement/If Else/Odd2.cpp
@@ -0,0 +1,16 @@
+//Odd Or Even - Version 2
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter number:";
+  cin>>n;
+  
+  if(n%2==1){
+    cout<<"Odd";
+  }else{
+    cout<<"Even";
+  }
+  return 0;
+}
