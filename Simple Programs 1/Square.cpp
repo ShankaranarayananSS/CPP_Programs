3dbacad718f789c6d63c82bc00a2b5dc719b33f1
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-28 23:50:30 +0530

    Create Shape

---
+++ Simple Programs 1/Square.cpp
@@ -0,0 +1,14 @@
+//Area of Circle      = S * S
+//Perimeter of Circle = 4 * S
+
+#include<iostream>
+using namespace std;
+int main(){
+  int s;
+  cout<<"Enter side of square:";
+  cin>>s;
+  
+  cout<<"\nArea of Square = "<<(s*s);
+  cout<<"\nPerimeter of Square = "<<(4*s);
+  return 0;
+}
