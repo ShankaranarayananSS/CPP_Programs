1ff7f59bdd89117bd78221c412b2a57f12bd4ad6
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-29 22:28:18 +0530

    Create Shape

---
+++ Simple Programs 1/Rectangle.cpp
@@ -0,0 +1,16 @@
+//Area of Rectangle      = L * B
+//Perimeter of Rectangle = 2 * (L + B)
+
+#include<iostream>
+using namespace std;
+int main(){
+  int l,b;
+  cout<<"Enter length of rectangle:";
+  cin>>l;
+  cout<<"Enter breadth of rectangle:";
+  cin>>b;
+  
+  cout<<"\nArea of Rectangle = "<<(l*b);
+  cout<<"\nPerimeter of Rectangle = "<<2*(l+b);
+  return 0;
+}
