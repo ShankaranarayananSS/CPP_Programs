feb4ad5061f56a035dbdadb322896be2392308d8
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-31 09:20:42 +0530

    Create Shape

---
+++ Simple Programs 1/Triangle.cpp
@@ -0,0 +1,14 @@
+//Area of Right Angle Triangle = 0.5 * B * H
+
+#include<iostream>
+using namespace std;
+int main(){
+  float b,h;
+  cout<<"Enter base value:";
+  cin>>b;
+  cout<<"Enter height value:";
+  cin>>h;
+  
+  cout<<"Area of Triangle = "<<0.5*b*h;
+  return 0;
+}
