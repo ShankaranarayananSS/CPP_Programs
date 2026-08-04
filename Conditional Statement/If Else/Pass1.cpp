68456b2fccb70912d7d75c63e30dbb9179324ac3
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-08-04 09:08:30 +0530

    Create Pass Or Fail Version 1

---
+++ Conditional Statement/If Else/Pass1.cpp
@@ -0,0 +1,16 @@
+//Pass Or Fail - Version 1
+
+#include<iostream>
+using namespace std;
+int main(){
+  int mark;
+  cout<<"Enter mark:";
+  cin>>mark;
+  
+  if(mark>=40){
+    cout<<"Pass";
+  }else{
+    cout<<"Fail";
+  }
+  return 0;
+}
