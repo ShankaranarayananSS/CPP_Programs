38189743c9847351d0f528aa54effde19454d73b
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-08-08 23:33:52 +0530

    Create Pattern

---
+++ Looping Statement/Nested/Pattern2.cpp
@@ -0,0 +1,21 @@
+/*
+EXPECTED PATTERN
+$  $  $  $  $
+$  $  $  $
+$  $  $
+$  $
+$
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+  int i,j;
+  for(i=1;i<=5;i++){
+    for(j=i;j<=5;j++){
+      cout<<"$\t";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
