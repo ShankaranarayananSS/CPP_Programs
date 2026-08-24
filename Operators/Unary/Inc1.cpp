546f38000aa99e71bea3868111da7d64c0fcbcec
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-08-24 23:00:19 +0530

    Create Increment Operator

---
+++ Operators/Unary/Inc1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int i=10;
+  cout<<"Pre Increment:";
+  
+  //Increases "i" value first, then displays it
+  cout<<endl<<"Step 1 = "<<++i;
+  cout<<endl<<"Step 2 = "<<i;
+  return 0;
+}
