a6aa7a6c605e5955f1ad332e9d0de7ef06197600
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-08-25 20:32:07 +0530

    Create Decrement Operator

---
+++ Operators/Unary/Dec1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int i=10;
+  cout<<"Pre Decrement:";
+  
+  //Decreases "i" value first, then displays it
+  cout<<endl<<"Step 1 = "<<--i;
+  cout<<endl<<"Step 2 = "<<i;
+  return 0;
+}
