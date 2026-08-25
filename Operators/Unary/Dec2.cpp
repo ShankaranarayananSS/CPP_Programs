db2ac2f7632592dee542e684886a4aa41fe19f65
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-08-25 20:34:47 +0530

    Create Decrement Operator

---
+++ Operators/Unary/Dec2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int i=10;
+  cout<<"Post Decrement:";
+  
+  //Displays "i" value first, then decreases it
+  cout<<endl<<"Step 1 = "<<i--;
+  cout<<endl<<"Step 2 = "<<i;
+  return 0;
+}
