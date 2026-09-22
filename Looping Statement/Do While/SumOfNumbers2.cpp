30b37c4e245c807967dfb5a92d565b9d589dcd39
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-09-22 10:35:13 +0530

    Create Sum Of Numbers

---
+++ Looping Statement/Do While/SumOfNumbers2.cpp
@@ -0,0 +1,17 @@
+//Sum of N Odd Numbers
+
+#include<iostream>
+using namespace std;
+int main(){
+  int i=1,n,add=0;
+  cout<<"Enter number:";
+  cin>>n;
+  
+  do{
+    add = add + i;
+    i = i + 2;
+  }
+  while(i<=n);
+  cout<<"Sum of Odd N numbers = "<<add;
+  return 0;
+}
